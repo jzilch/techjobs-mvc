e7a789b1760f6f4ef92b516d5a8279d70a24cc78
jake zilch <jzilch280@gmail.com>
2017-06-26 15:06:20 -0500

    Started over from scratch

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -1,6 +1,5 @@
 package org.launchcode.controllers;
 
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,10 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.lang.model.type.ArrayType;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+
 
 /**
  * Created by LaunchCode
@@ -28,27 +26,20 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
-    @RequestMapping(value = "", method = RequestMethod.POST)
-    public String search(Model model, HttpServletRequest request)   {
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
         model.addAttribute("columns", ListController.columnChoices);
+        String column = searchType;
+        String value = searchTerm;
 
-        String searchType = request.getParameter("searchType");
-        String searchTerm = request.getParameter("searchTerm");
-        ArrayList<HashMap<String, String>> allJobs = JobData.findAll();
-        ArrayList<HashMap<String, String>> searchedJobs = new ArrayList<>();
-
-        if (searchType.equals("all"))   {
-            searchedJobs = JobData.findByValue(searchTerm);
+        if  (column.equals("all"))  {
+            ArrayList<HashMap<String, String>> searchedJobs = JobData.findByValue(value);
+            model.addAttribute("jobs", searchedJobs);
         }   else    {
-            searchedJobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            ArrayList<HashMap<String, String>> searchedJobs = JobData.findByColumnAndValue(column, value);
+            model.addAttribute("jobs", searchedJobs);
         }
-
-        int Entries = 0;
-        for (HashMap<String, String> number : searchedJobs) {
-            Entries = Entries + 1;
-        }
-        model.addAttribute("jobs", searchedJobs);
-        model.addAttribute("Entries", (Entries + "Result(s)"));
         return "search";
     }
 
